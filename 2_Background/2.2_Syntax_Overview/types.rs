972ca7a573ac30c441f90a094f5830b9d34d207c
Glenn <130987493+G-L-E-N-N@users.noreply.github.com>
2025-07-12 12:06:04 +0200

    Create types.rs

---
+++ 2_Background/2.2_Syntax_Overview/types.rs
@@ -0,0 +1,10 @@
+fn types() {
+    let x = 42;         // type inferred as i32
+    let flag = true;    // inferred as bool
+
+    let y: f64 = 3.14;  // explicit type declaration
+
+    println!("x = {}", x);
+    println!("flag = {}", flag);
+    println!("y = {}", y);
+}

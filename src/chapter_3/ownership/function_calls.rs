2521dd435e30db29cafc499234d35861abb4521f
Glenn <130987493+G-L-E-N-N@users.noreply.github.com>
2025-07-12 15:47:35 +0200

    Create function_calls.rs

---
+++ src/chapter_3/ownership/function_calls.rs
@@ -0,0 +1,10 @@
+pub fn pass_by_value() {
+    let order = String::from("Order #1234: 2x Coffee");
+    send_order(order); // Ownership is moved to the function
+
+    // println!("{}", order); // ERROR: order was moved and is no longer valid
+}
+
+fn send_order(order: String) { // Ownership of order is moved here
+    println!("Sending order: {}", order); // Processing the moved order string
+}

63149006a60b6c0b0942acef2e5c798cee50e1d5
Glenn <130987493+G-L-E-N-N@users.noreply.github.com>
2025-07-12 13:12:13 +0200

    Create references.rs

---
+++ src/2_Background/2_2_Syntax_Overview/references.rs
@@ -0,0 +1,10 @@
+pub fn references() {
+    let w = 10;
+    let x = &w;  // x is a reference to w
+    println!("Value via reference: {}", x);
+
+    let mut y = 5;
+    let z = &mut y;  // z is a mutable reference to y
+    *z += 1;         // dereference z to change the value
+    println!("y is now: {}", y);
+}

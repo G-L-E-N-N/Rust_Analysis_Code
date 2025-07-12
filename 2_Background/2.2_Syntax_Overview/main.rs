9440b7a42b8a9b90cb0d1a2901cb1c433a7f3417
Glenn <130987493+G-L-E-N-N@users.noreply.github.com>
2025-07-12 12:30:10 +0200

    Create main.rs

---
+++ 2_Background/2.2_Syntax_Overview/main.rs
@@ -0,0 +1,18 @@
+mod control_flow_loops;
+mod functions;
+mod printing;
+mod references;
+mod structs;
+mod types;
+mod variables;
+
+fn main() {
+  control_flow_loops::control_flow();
+  functions::functions();
+  printing::printing();
+  references::references();
+  structs::structs();
+  types::types();
+  variables::variables();
+}
+  

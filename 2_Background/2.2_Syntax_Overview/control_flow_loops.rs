3ecae75c189013978d103060c858d0e7fe5abaa8
Glenn <130987493+G-L-E-N-N@users.noreply.github.com>
2025-07-12 13:14:04 +0200

    Delete 2_Background/2.2_Syntax_Overview directory

--- 2_Background/2.2_Syntax_Overview/control_flow_loops.rs
+++
@@ -1,9 +0,0 @@
-pub fn control_flow() {
-    for number in 1..=5 {
-        if number % 2 == 0 {
-            println!("{} is even", number);
-        } else {
-            println!("{} is odd", number);
-        }
-    }
-}
--- 2_Background/2.2_Syntax_Overview/structs.rs
+++
@@ -1,9 +0,0 @@
-struct Point {
-    x: f64,
-    y: f64,
-}
-
-pub fn structs() {
-    let p = Point { x: 3.0, y: 4.0 };
-    println!("x = {}, y = {}", p.x, p.y);
-}

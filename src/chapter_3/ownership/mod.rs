67007b3f9a826741cff85a690c0241e550df8410
Glenn <130987493+G-L-E-N-N@users.noreply.github.com>
2025-07-12 15:37:52 +0200

    Update mod.rs

--- src/chapter_3/ownership/mod.rs
+++ src/chapter_3/ownership/mod.rs
@@ -1,6 +1,9 @@
 mod moving;
+mod copy_types;
 
 pub fn ownership_main() {
     moving::ownership_move_simple();
     moving::ownership_move_conditional();
+    copy_types::example_copy_temperature();
+    copy_types::example_copy_simple();
 }

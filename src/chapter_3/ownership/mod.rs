9ba3afa4c52d69adce6a28af1fd403fcefa6d2de
Glenn <130987493+G-L-E-N-N@users.noreply.github.com>
2025-07-12 15:42:51 +0200

    Update mod.rs

--- src/chapter_3/ownership/mod.rs
+++ src/chapter_3/ownership/mod.rs
@@ -1,9 +1,13 @@
 mod moving;
 mod copy_types;
+mod drop;
 
 pub fn ownership_main() {
     moving::ownership_move_simple();
     moving::ownership_move_conditional();
     copy_types::example_copy_temperature();
     copy_types::example_copy_simple();
+    drop::drop_scope_simple();
+    drop::drop_scope_block();
+    drop::drop_password_prompt();
 }

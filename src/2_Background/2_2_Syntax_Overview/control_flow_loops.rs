a67f8e25bdadff3056644df5b19f5f294da48193
Glenn <130987493+G-L-E-N-N@users.noreply.github.com>
2025-07-12 13:09:57 +0200

    Create control_flow_loops.rs

---
+++ src/2_Background/2_2_Syntax_Overview/control_flow_loops.rs
@@ -0,0 +1,9 @@
+pub fn control_flow() {
+    for number in 1..=5 {
+        if number % 2 == 0 {
+            println!("{} is even", number);
+        } else {
+            println!("{} is odd", number);
+        }
+    }
+}

acbfddf571e30fa9382cbab172cdfe443f85ee7b
G-L-E-N-N <Glenn.Hannig@gmail.com>
2025-07-13 11:25:31 +0200

    Renamed folder

---
+++ target/debug/deps/rust_analysis_code.d
@@ -0,0 +1,21 @@
+C:\Users\valar\Rust_Analysis_Code\target\debug\deps\rust_analysis_code.exe: src\main.rs src\chapter_2\mod.rs src\chapter_2\syntax_overview\mod.rs src\chapter_2\syntax_overview\control_flow_loops.rs src\chapter_2\syntax_overview\functions.rs src\chapter_2\syntax_overview\printing.rs src\chapter_2\syntax_overview\references.rs src\chapter_2\syntax_overview\structs.rs src\chapter_2\syntax_overview\types.rs src\chapter_2\syntax_overview\variables.rs src\chapter_3\mod.rs src\chapter_3\ownership\mod.rs src\chapter_3\ownership\moving.rs src\chapter_3\ownership\copy_types.rs src\chapter_3\ownership\drop.rs src\chapter_3\ownership\function_calls.rs src\chapter_3\borrowing\mod.rs
+
+C:\Users\valar\Rust_Analysis_Code\target\debug\deps\rust_analysis_code.d: src\main.rs src\chapter_2\mod.rs src\chapter_2\syntax_overview\mod.rs src\chapter_2\syntax_overview\control_flow_loops.rs src\chapter_2\syntax_overview\functions.rs src\chapter_2\syntax_overview\printing.rs src\chapter_2\syntax_overview\references.rs src\chapter_2\syntax_overview\structs.rs src\chapter_2\syntax_overview\types.rs src\chapter_2\syntax_overview\variables.rs src\chapter_3\mod.rs src\chapter_3\ownership\mod.rs src\chapter_3\ownership\moving.rs src\chapter_3\ownership\copy_types.rs src\chapter_3\ownership\drop.rs src\chapter_3\ownership\function_calls.rs src\chapter_3\borrowing\mod.rs
+
+src\main.rs:
+src\chapter_2\mod.rs:
+src\chapter_2\syntax_overview\mod.rs:
+src\chapter_2\syntax_overview\control_flow_loops.rs:
+src\chapter_2\syntax_overview\functions.rs:
+src\chapter_2\syntax_overview\printing.rs:
+src\chapter_2\syntax_overview\references.rs:
+src\chapter_2\syntax_overview\structs.rs:
+src\chapter_2\syntax_overview\types.rs:
+src\chapter_2\syntax_overview\variables.rs:
+src\chapter_3\mod.rs:
+src\chapter_3\ownership\mod.rs:
+src\chapter_3\ownership\moving.rs:
+src\chapter_3\ownership\copy_types.rs:
+src\chapter_3\ownership\drop.rs:
+src\chapter_3\ownership\function_calls.rs:
+src\chapter_3\borrowing\mod.rs:

be812ef900b3b62c9523b4c2a39e7c219c8fb005
Glenn <130987493+G-L-E-N-N@users.noreply.github.com>
2025-07-13 11:01:11 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ mod chapter_2;
 mod chapter_3;
 
 fn main() {
-    //chapter_2::syntax_overview::background_main();
+    chapter_2::syntax_overview::background_main();
     chapter_3::ownership::ownership_main();
 }

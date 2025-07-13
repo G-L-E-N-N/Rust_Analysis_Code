becce6e687a5a3e6618c97c8b938517a9da4993d
Glenn <130987493+G-L-E-N-N@users.noreply.github.com>
2025-07-13 12:21:11 +0200

    Update mod.rs

--- src/chapter_3/borrowing/mod.rs
+++ src/chapter_3/borrowing/mod.rs
@@ -1,6 +1,6 @@
 mod immutable_borrows;
 mod mutable_borrows;
-//mod dangling_references;
+mod dangling_references;
 mod function_calls;
 
 pub fn borrowing_main(){
@@ -9,7 +9,7 @@ pub fn borrowing_main(){
   mutable_borrows::mutable_borrow_and_modify();
   mutable_borrows::multiple_mutable_borrows_illegal();
   mutable_borrows::sorting_mut_slice_example();
-  //dangling_references::dangling_reference_example();
+  dangling_references::dangling_reference_example();
   function_calls::pass_by_reference();
   function_calls::multiple_pass_by_reference();
 }

09ed8ac9a88445c76da290c1726ec32544e849d1
Glenn <130987493+G-L-E-N-N@users.noreply.github.com>
2025-07-13 13:37:25 +0200

    Delete src/main.rs

--- src/main.rs
+++
@@ -1,8 +0,0 @@
-mod chapter_2;
-mod chapter_3;
-
-fn main() {
-    chapter_2::syntax_overview::background_main();
-    chapter_3::ownership::ownership_main();
-    chapter_3::borrowing::borrowing_main();
-}

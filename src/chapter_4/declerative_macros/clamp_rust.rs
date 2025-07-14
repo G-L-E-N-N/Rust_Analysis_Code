db2738fb104542c164458b9d10383bf723d5a500
Glenn <130987493+G-L-E-N-N@users.noreply.github.com>
2025-07-14 11:41:59 +0200

    Update clamp_rust.rs

--- src/chapter_4/declerative_macros/clamp_rust.rs
+++ src/chapter_4/declerative_macros/clamp_rust.rs
@@ -1,26 +1,30 @@
-macro_rules! clamp {
-    ($x:expr, $low:expr, $high:expr) => {{
-        let val = $x;
-        if val < $low {
-            $low
-        } else if val > $high {
-            $high
-        } else {
-            val
-        }
+fn foo(x: &mut i32) -> i32 {
+    *x += 1;
+    println!("foo called, x is now {}", x);
+    *x
+}
+
+fn bar(y: &mut i32) -> i32 {
+    *y += 2;
+    println!("bar called, y is now {}", y);
+    *y
+}
+
+macro_rules! min {
+    ($x:expr, $y:expr) => {{
+        let x_val = $x;
+        let y_val = $y;
+        if x_val < y_val { x_val } else { y_val }
     }};
 }
 
-fn get_val(val: &mut i32) -> i32 {
-    *val += 1;
-    *val
-}
-
 fn main() {
-    let mut val = 0;
+    let mut a = 5;
+    let mut b = 5;
 
-    let result = clamp!(get_val(&mut val), 2, 5);
+    let result = min!(foo(&mut a), bar(&mut b));
 
-    println!("Result: {}, val: {}", result, val);
+    println!("Result: {}", result);
+    println!("Final a: {}", a);
+    println!("Final b: {}", b);
 }
-

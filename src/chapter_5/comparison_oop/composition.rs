0566d6256fa871c92394c39bbddc7e52a83f79f9
Glenn <130987493+G-L-E-N-N@users.noreply.github.com>
2025-07-14 14:38:31 +0200

    Create composition.rs

---
+++ src/chapter_5/comparison_oop/composition.rs
@@ -0,0 +1,14 @@
+// Rust: Using traits and composition
+trait Shape {
+    fn area(&self) -> f64;
+}
+
+struct Circle {
+    radius: f64,
+}
+
+impl Shape for Circle {
+    fn area(&self) -> f64 {
+        3.14 * self.radius * self.radius
+    }
+}

6cf9043df9ee350e3c9c01f96f2efd9c44cb808b
Glenn <130987493+G-L-E-N-N@users.noreply.github.com>
2025-07-14 11:36:01 +0200

    Update clamp_c.c

--- src/chapter_4/declerative_macros/clamp_c.c
+++ src/chapter_4/declerative_macros/clamp_c.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 
-int get_value(int *counter) {
-    (*counter) += 1;
-    return *counter; // Rückgabe ist aktueller Wert nach Inkrement
+int foo(int *z) {
+    (*z)++;
+    printf("foo called, z now = %d\n", *z);
+    return *z;
 }
 
-#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define min(X, Y)  ((X) < (Y) ? (X) : (Y))
 
 int main() {
-    int count = 0;
-    int result = MAX(get_value(&count), 3);
-    printf("Result: %d\n", result);
-    printf("Call count: %d\n", count);
+    int z = 5;
+    int x = 2;
+    int y = 3;
+
+    int next = min(x + y, foo(&z));  // foo wird evtl. 2x aufgerufen!
+
+    printf("next = %d\n", next);
+    printf("z = %d\n", z);
+
     return 0;
 }

11f80cdac0331d6a39f4fd7dcae3177167f1b9de
Glenn <130987493+G-L-E-N-N@users.noreply.github.com>
2025-07-15 11:53:16 +0200

    Update min_cpp.cpp

--- src/chapter_4/declerative_macros/min_cpp.cpp
+++ src/chapter_4/declerative_macros/min_cpp.cpp
@@ -19,8 +19,6 @@ int main() {
     int brand = 20;
     int non_brand = 14;
 
-    // Die Funktionen werden hier jeweils genau einmal aufgerufen,
-    // weil zuerst die Argumente ausgewertet werden
     int cheaper = min(normal_discount(non_brand), premium_discount(brand));
 
     std::cout << "The cheaper one will cost: " << cheaper << std::endl;

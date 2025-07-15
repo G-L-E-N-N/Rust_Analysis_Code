a3e9045da2cc7b8072620702639e4e4a507cff32
Glenn <130987493+G-L-E-N-N@users.noreply.github.com>
2025-07-15 11:50:30 +0200

    Update min_rust.rs

--- src/chapter_4/declerative_macros/min_rust.rs
+++ src/chapter_4/declerative_macros/min_rust.rs
@@ -1,3 +1,11 @@
+macro_rules! min {
+    ($a:expr, $b:expr) => {{
+        let temp_a = $a;
+        let temp_b = $b;
+        if temp_a < temp_b { temp_a } else { temp_b }
+    }};
+}
+
 fn normal_discount(non_brand: &mut i32) -> i32 {
     *non_brand -= 2;
     *non_brand
@@ -7,15 +15,6 @@ fn premium_discount(brand: &mut i32) -> i32 {
     *brand -= 5;
     *brand
 }
-
-macro_rules! min {
-    ($a:expr, $b:expr) => {{
-        let temp_a = $a;
-        let temp_b = $b;
-        if temp_a < temp_b { temp_a } else { temp_b }
-    }};
-}
-
 
 fn main() {
     let mut brand = 20;

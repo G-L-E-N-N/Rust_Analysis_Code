47a63e2d9ff2b4bad98fd48050e259c027ebaa4a
Glenn <130987493+G-L-E-N-N@users.noreply.github.com>
2025-07-15 15:03:27 +0200

    Update dynamic_dispatch_cpp.cpp

--- src/chapter_5/comparison_oop/dynamic_dispatch_cpp.cpp
+++ src/chapter_5/comparison_oop/dynamic_dispatch_cpp.cpp
@@ -1,4 +1,28 @@
+#include <iostream>
+
+// C++: Using inheritance for abstraction
+class Shape {
+public:
+    virtual double area() const = 0;
+    virtual ~Shape() = default;
+};
+
+class Circle : public Shape {
+    double radius;
+public:
+    Circle(double r) : radius(r) {}
+    double area() const override {
+        return 3.14 * radius * radius;
+    }
+};
+
 // C++: Polymorphism with virtual methods
 void print_area(Shape* s) {
-    std::cout << s->area();
+    std::cout << "Area: " << s->area() << std::endl;
+}
+
+int main() {
+    Circle c(5.0);
+    print_area(&c); 
+    return 0;
 }

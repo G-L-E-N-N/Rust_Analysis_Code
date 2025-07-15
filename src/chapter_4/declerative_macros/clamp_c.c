92a6700a748d549ee1a8628387bdf7c3806d8007
Glenn <130987493+G-L-E-N-N@users.noreply.github.com>
2025-07-15 11:39:28 +0200

    Update clamp_c.c

--- src/chapter_4/declerative_macros/clamp_c.c
+++ src/chapter_4/declerative_macros/clamp_c.c
@@ -2,34 +2,23 @@
 
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 
-void apply_membership_discount(int* price) {
-    printf("Applying membership discount\n");
-    *price -= 2;
-}
-
-void apply_promotion_discount(int* price) {
-    printf("Applying promotion discount\n");
-    *price -= 5;
-}
-
 int membership_discount(int* price) {
-    apply_membership_discount(price);
+    *price -= 2;
     return *price;
 }
 
 int promotion_discount(int* price) {
-    apply_promotion_discount(price);
+    *price -= 5;
     return *price;
 }
 
 int main() {
-    int base_price1 = 20;
-    int base_price2 = 20;
+    int price = 20;
 
-    int final_price = MIN(membership_discount(&base_price1), promotion_discount(&base_price2));
+    int final_price = MIN(membership_discount(&price), promotion_discount(&price));
 
     printf("Final price: %d\n", final_price);
-    printf("Aftermath: base_price1 = %d, base_price2 = %d\n", base_price1, base_price2);
+    printf("Remaining price: %d\n", price);
 
     return 0;
 }

87317a57851ae67c7c618ea96e7c89facbcb0442
Glenn <130987493+G-L-E-N-N@users.noreply.github.com>
2025-07-15 12:00:21 +0200

    Update min_cpp.cpp

--- src/chapter_4/declerative_macros/min_cpp.cpp
+++ src/chapter_4/declerative_macros/min_cpp.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
 
-int normal_discount(int& non_brand) {
-    non_brand -= 2;
-    return non_brand;
-}
-
-int premium_discount(int& brand) {
-    brand -= 5;
-    return brand;
-}
-
+// Template-Funktion, die zwei Werte vom Typ T vergleicht und den kleineren zurückgibt
 template<typename T>
-T min(T a, T b) {
+T min_template(T a, T b) {
     return (a < b) ? a : b;
+}
+
+int normal_discount(int* non_brand) {
+    *non_brand -= 2;
+    return *non_brand;
+}
+
+int premium_discount(int* brand) {
+    *brand -= 5;
+    return *brand;
 }
 
 int main() {
     int brand = 20;
     int non_brand = 14;
 
-    int cheaper = min(normal_discount(non_brand), premium_discount(brand));
+    // Aufruf der Discount-Funktionen mit Zeigern, Rückgabe der veränderten Werte
+    int cheaper = min_template(normal_discount(&non_brand), premium_discount(&brand));
 
     std::cout << "The cheaper one will cost: " << cheaper << std::endl;
 

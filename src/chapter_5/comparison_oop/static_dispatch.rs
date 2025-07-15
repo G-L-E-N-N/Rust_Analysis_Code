92cdcc7a94d3ca92bf772bbb651f6c24917f4c09
Glenn <130987493+G-L-E-N-N@users.noreply.github.com>
2025-07-15 14:58:34 +0200

    Update static_dispatch.rs

--- src/chapter_5/comparison_oop/static_dispatch.rs
+++ src/chapter_5/comparison_oop/static_dispatch.rs
@@ -1,4 +1,25 @@
+// Rust: Using traits and composition
+trait Shape {
+    fn area(&self) -> f64;
+}
+
+struct Circle {
+    radius: f64,
+}
+
+impl Shape for Circle {
+    fn area(&self) -> f64 {
+        3.14 * self.radius * self.radius
+    }
+}
+
 // Rust: Static dispatch via generics
 fn print_area<T: Shape>(shape: &T) {
     println!("{}", shape.area());
+}
+
+fn main() {
+    let circle = Circle { radius: 5.0 };
+    print!("Circle area: ");
+    print_area(&circle); // Statischer Dispatch zur Compile-Zeit
 }

1b3a00a0d1afb22367adc9fe5b9ee3fc17b7fa29
Glenn <130987493+G-L-E-N-N@users.noreply.github.com>
2025-07-16 13:07:33 +0200

    Update lifetimes.rs

--- src/chapter_3/borrowing/lifetimes.rs
+++ src/chapter_3/borrowing/lifetimes.rs
@@ -1,9 +1,28 @@
+// Function demonstrating automatic lifetime elision
 fn first_char(s: &str) -> &str {
     &s[0..1]
 }
 
+// Function demonstrating explicit lifetime annotation
+fn longest<'a>(s1: &'a str, s2: &'a str) -> &'a str {
+    if s1.len() > s2.len() {
+        s1
+    } else {
+        s2
+    }
+}
+
 fn main() {
+
+    // Using automatic lifetime elision
     let word = String::from("Rust");
     let ch = first_char(&word);
     println!("The first character is: {}", ch);
+
+    // Using explicit lifetime annotations
+    let string1 = String::from("Hallo");
+    let string2 = String::from("Welt!");
+
+    let result = longest(&string1, &string2);
+    println!("The longer string is: {}", result);
 }

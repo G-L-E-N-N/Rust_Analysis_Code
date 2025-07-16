4824ff034206920ea2a361851fab9aef8f76fd6d
Glenn <130987493+G-L-E-N-N@users.noreply.github.com>
2025-07-16 17:35:41 +0200

    Update attempted_rust_trans.rs

--- src/chapter_3/cyclic_data_structures/attempted_rust_trans.rs
+++ src/chapter_3/cyclic_data_structures/attempted_rust_trans.rs
@@ -1,52 +1,63 @@
 struct Node {
     value: i32,
-    next: *mut Node, // raw pointer for manual cyclic reference
-}
-
-impl Node {
-    fn new(val: i32) -> *mut Node {
-        let node = Box::new(Node {
-            value: val,
-            next: std::ptr::null_mut(),
-        });
-        Box::into_raw(node) // ownership now unmanaged
-    }
+    next: Option<*mut Node>, // raw pointer!
 }
 
 struct CyclicList {
-    head: *mut Node,
+    head: Option<*mut Node>,
 }
 
 impl CyclicList {
     fn new() -> Self {
-        CyclicList {
-            head: std::ptr::null_mut(),
-        }
+        CyclicList { head: None }
     }
 
     fn add_node(&mut self, val: i32) {
-        let new_node = Node::new(val);
+        // allocate node on the heap manually (like new in C++)
+        let new_node = Box::into_raw(Box::new(Node { value: val, next: None }));
 
-        if self.head.is_null() {
-            self.head = new_node;
-            (*new_node).next = self.head; // Error 1: raw pointer to self
-        } else {
-            let mut temp = self.head;
-            while !(*temp).next.is_null() && (*temp).next != self.head {
-                temp = (*temp).next;
+        unsafe {
+            if self.head.is_none() {
+                (*new_node).next = Some(new_node); // self-cycle
+                self.head = Some(new_node);
+            } else {
+                let mut temp = self.head.unwrap();
+                while let Some(next_ptr) = (*temp).next {
+                    if next_ptr == self.head.unwrap() {
+                        break;
+                    }
+                    temp = next_ptr;
+                }
+
+                (*temp).next = Some(new_node);
+                (*new_node).next = self.head;
             }
-            (*temp).next = new_node;
-            (*new_node).next = self.head; // Error 2: forming a cycle manually
         }
     }
 }
-
 
 fn main() {
     let mut list = CyclicList::new();
 
-    list.add_node(1);
-    list.add_node(2);
-    list.add_node(3);
+    list.add_node(10);
+    list.add_node(20);
+    list.add_node(30);
+
+    unsafe {
+        if let Some(mut current) = list.head {
+            // Wir wissen, dass wir 3 Knoten eingefügt haben
+            for _ in 0..3 {
+                println!("{}", (*current).value);
+                // Nächster Knoten
+                current = match (*current).next {
+                    Some(next_ptr) => next_ptr,
+                    None => break,
+                };
+            }
+        } else {
+            println!("Liste ist leer");
+        }
+    }
 }
+
 

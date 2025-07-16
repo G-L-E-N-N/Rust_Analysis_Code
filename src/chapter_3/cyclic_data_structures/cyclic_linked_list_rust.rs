4b5b15ae5c04168cdb8f84fefd331898c6c21447
Glenn <130987493+G-L-E-N-N@users.noreply.github.com>
2025-07-16 18:19:02 +0200

    Update cyclic_linked_list_rust.rs

--- src/chapter_3/cyclic_data_structures/cyclic_linked_list_rust.rs
+++ src/chapter_3/cyclic_data_structures/cyclic_linked_list_rust.rs
@@ -3,12 +3,12 @@ use std::cell::RefCell;
 
 struct Node {
     value: i32,
-    next: Option<Rc<RefCell<Node>>>,
+    next: Option<Rc<RefCell<Node>>>, // Next node wrapped in Rc<RefCell> for shared mutable ownership
 }
 
 struct CyclicList {
-    head: Option<Rc<RefCell<Node>>>,
-    tail: Option<Weak<RefCell<Node>>>,
+    head: Option<Rc<RefCell<Node>>>, // Start of the cyclic list
+    tail: Option<Weak<RefCell<Node>>>, // Weak reference to the last node to avoid reference cycles
 }
 
 impl CyclicList {
@@ -17,24 +17,30 @@ impl CyclicList {
     }
 
     fn add_node(&mut self, val: i32) {
+        // Create a new node wrapped in Rc<RefCell> for shared ownership and interior mutability
         let new_node = Rc::new(RefCell::new(Node { value: val, next: None }));
 
         if let Some(head) = self.head.clone() {
+            // If the list is not empty, update the current tail to point to the new node
             if let Some(tail_weak) = &self.tail {
                 if let Some(tail) = tail_weak.upgrade() {
                     tail.borrow_mut().next = Some(new_node.clone());
                 }
             }
+            // New node points back to the head to maintain the cycle
             new_node.borrow_mut().next = Some(head);
+            // Update tail to be the new node (as a weak reference)
             self.tail = Some(Rc::downgrade(&new_node));
         } else {
-            // Erste Node, die auf sich selbst zeigt
+            // First node in the list points to itself to form a cycle
             new_node.borrow_mut().next = Some(new_node.clone());
+            // Head and tail both point to the new node
             self.head = Some(new_node.clone());
             self.tail = Some(Rc::downgrade(&new_node));
         }
     }
 }
+
 
 
 fn main() {

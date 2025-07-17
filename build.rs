81f917dee99a0dce55fda8b59acd3ad73f0861d8
Glenn <130987493+G-L-E-N-N@users.noreply.github.com>
2025-07-17 13:22:50 +0200

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
 fn main() {
     cc::Build::new()
         .file("src/chapter_5/interfacing/multiply.c")
-        .compile("libmultiply.a");
+        .file("src/chapter_5/interfacing/dot_product.c")
+        .compile("libmylib.a");
 }
